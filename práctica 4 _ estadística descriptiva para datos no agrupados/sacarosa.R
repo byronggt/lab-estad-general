00d787a695166856732fca579d403d4d6793d8fe
ByronG <byrong.gt@gmail.com>
2024-01-09 10:07:53 -0600

    Actualizada la práctica de datos sin agrupar

---
+++ práctica 4 _ estadística descriptiva para datos no agrupados/sacarosa.R
@@ -0,0 +1,26 @@
+# Dr. Byron González
+# Dr. Ezequiel López
+# http://cete.fausac.gt
+
+### Lectura de bases de datos y medidas de resumen estadístico ####
+
+# Colocar en memoria las bibliotecas a usar
+if(!require(readxl)){install.packages("readxl")}
+if(!require(dplyr)){install.packages("dplyr")}
+
+# Abrir la base de datos
+# No olvidar el cambio de directorio de trabajo
+sacarosa<-read_excel("sacarosa.xlsx")
+head(sacarosa)
+attach(sacarosa)
+
+# Box plot de ph por muestra
+boxplot(ph~muestra)
+
+# Promedio de ph, desviación estándar e IQR por muestra
+resumen <- sacarosa %>%
+  group_by(muestra) %>%
+  summarise(Media = mean(ph),
+            Desviación = sd(ph),
+            IQR = IQR(ph))
+resumen
